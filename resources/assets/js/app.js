e705581d234c5b9fb8286ea2d8614fd4b842c56b
Michael Dolphin <michael@opencopy.com.au>
2018-09-08 13:43:17 +0800

    google map basics

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,6 +7,7 @@
 
 require('./bootstrap');
 
+
 window.Vue = require('vue');
 
 /**
@@ -15,8 +16,63 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+require('./components/map.js');
 
 const app = new Vue({
     el: '#app'
 });
+
+var mapContainer = document.getElementById('map');
+var mapInput = document.getElementById('autocomplete');
+
+window.initMap = function(){
+	map = new google.maps.Map(mapContainer, 
+		{
+		center: {
+			lat: -34.397,
+			lng: 150.644
+		},
+		zoom: 8
+    });
+
+    var input = mapInput;
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+
+    var marker = new google.maps.Marker({
+      map: map,
+      anchorPoint: new google.maps.Point(-34.397, 150.644)
+    });
+
+    autocomplete.addListener('place_changed', function() {
+      
+      marker.setVisible(false);
+      var place = autocomplete.getPlace();
+      if (!place.geometry) {
+        // User entered the name of a Place that was not suggested and
+        // pressed the Enter key, or the Place Details request failed.
+        window.alert("No details available for input: '" + place.name + "'");
+        return;
+      }
+
+      // If the place has a geometry, then present it on a map.
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);  // Why 17? Because it looks good.
+      }
+      marker.setPosition(place.geometry.location);
+      marker.setVisible(true);
+
+      var address = '';
+      if (place.address_components) {
+        address = [
+          (place.address_components[0] && place.address_components[0].short_name || ''),
+          (place.address_components[1] && place.address_components[1].short_name || ''),
+          (place.address_components[2] && place.address_components[2].short_name || '')
+        ].join(' ');
+      }
+
+    });
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Laravel</title>
 
@@ -12,11 +13,22 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
-    <header class="full-height text-center flex-center">
-        <h1>seascape</h1>
-    </header>
-    <div class="row">
+        <div id="app">
 
-    </div>
+            <header class="full-height text-center flex-center">
+                <h1>seascape</h1>
+            </header>
+            <div class="row">
+
+            </div>
+
+            @include('map')
+
+        </div>
+
+        <script src="{{asset('js/app.js')}}"></script>
+
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDmRcarfXPtbpOPF1QAcUTvmcAuiSVVYcw&libraries=places&callback=initMap"
+        async defer></script>
     </body>
 </html>
