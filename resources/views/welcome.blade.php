e379822b5e6d4edac5c0331ac22a21220ff04356
Emilian Roman <roman.emilian@outlook.com>
2018-09-08 20:11:33 +0800

    Add headers to the home page, update layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,22 +20,26 @@
             <header class="full-height text-center p-5 header parallax-window"
                     data-parallax="scroll"
                     data-image-src="/background.jpg">
-                <div class="container-fluid">
+                <div class="container-fluid text-light">
                     <div class="row">
                         <div class="col-md-12">
                             <img src="{{ asset('logo.png') }}" alt="">
                         </div>
                     </div>
                     <div class="row my-5">
-                        <div class="col-md-12">
+                        <div class="col-md-12 ">
+                            <h4>How does pollution impact our air, our oceans, and us in urban and regional areas?</h4>
+                            <h5>With open government data, we help you understand our atmosphere and hydrosphere.</h5>
+                        </div>
+                        <div class="col-md-12 mt-5">
                             <a href="{{ route('login') }}" class="btn btn-outline-light btn-250 btn-radius-left">Login</a>
                             <a href="#mapContainer" class="btn btn-outline-light btn-250 rounded-0">Try it!</a>
-                            <a href="{{ route('register') }}" class="btn btn-outline-light btn-250 btn-radius-right text-light">Register</a>
+                            <a href="{{ route('register') }}" class="btn btn-outline-light btn-250 btn-radius-right">Register</a>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-md-12">
-                            <p class="text-light text-monospace">
+                        <div class="col-md-12 mt-5">
+                            <p class="text-monospace">
                                 <small>
                                     GH:\> isobelle mead \ emilian roman \ michael dolphin \ ryan \ jason _
                                 </small>
