4be754bf92ad85d58e13dd9192025f135f6b9be9
Michael Dolphin <michael@opencopy.com.au>
2018-09-08 22:45:10 +0800

    results data

--- public/js/app.js
+++ public/js/app.js
@@ -47186,7 +47186,111 @@ exports.clearImmediate = (typeof self !== "undefined" && self.clearImmediate) ||
 /* 39 */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: Error: EPROTO: protocol error, read\n    at Error (native)");
+Vue.component('seascape-map', {
+
+	props: [],
+
+	data: function data() {
+		return {
+			location: {},
+			results: null
+		};
+	},
+
+
+	methods: {
+		initMap: function initMap() {
+
+			var self = this;
+
+			map = new google.maps.Map(document.getElementById('map'), {
+				center: {
+					lat: -34.397,
+					lng: 150.644
+				},
+				zoom: 8
+			});
+
+			var input = document.getElementById('autocomplete');
+
+			var autocomplete = new google.maps.places.Autocomplete(input);
+
+			var marker = new google.maps.Marker({
+				map: map,
+				anchorPoint: new google.maps.Point(-34.397, 150.644)
+			});
+
+			autocomplete.addListener('place_changed', function () {
+
+				marker.setVisible(false);
+				var place = autocomplete.getPlace();
+				if (!place.geometry) {
+					// User entered the name of a Place that was not suggested and
+					// pressed the Enter key, or the Place Details request failed.
+					window.alert("No details available for input: '" + place.name + "'");
+					return;
+				}
+
+				// If the place has a geometry, then present it on a map.
+				if (place.geometry.viewport) {
+					map.fitBounds(place.geometry.viewport);
+				} else {
+					map.setCenter(place.geometry.location);
+					map.setZoom(17); // Why 17? Because it looks good.
+				}
+				marker.setPosition(place.geometry.location);
+				marker.setVisible(true);
+
+				var address = '';
+				if (place.address_components) {
+					address = [place.address_components[0] && place.address_components[0].short_name || '', place.address_components[1] && place.address_components[1].short_name || '', place.address_components[2] && place.address_components[2].short_name || ''].join(' ');
+				}
+
+				self.location = place;
+				self.searchLocation();
+			});
+		},
+		searchLocation: function searchLocation() {
+
+			var self = this;
+
+			var form = self.$refs.searchForm;
+			var formData = new FormData(form);
+
+			formData.append('lat', self.location.geometry.location.lat());
+			formData.append('lng', self.location.geometry.location.lng());
+
+			axios.post('/vue/search/store', formData).then(function (response) {
+
+				console.log(response.data[0]);
+
+				self.results = response.data;
+			}).catch(function (error) {
+
+				console.log(error);
+			});
+		}
+	},
+
+	mounted: function mounted() {},
+
+	computed: {},
+
+	updated: function updated() {},
+
+	watch: {
+		location: function location() {
+			var part1 = this.location.address_components[0].long_name;
+			var part2 = this.location.address_components[2].long_name;
+
+			var msg = new SpeechSynthesisUtterance('You have searched for ' + part1 + ' ' + part2);
+			window.speechSynthesis.speak(msg);
+
+			// window.speechSynthesis.speak(msg);
+		}
+	}
+
+});
 
 /***/ }),
 /* 40 */
