63a0d48b670d4eda2edef3a1c7379705555b7032
Emilian Roman <roman.emilian@outlook.com>
2018-09-09 12:25:23 +0800

    Discard modal and show results

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -1,10 +1,10 @@
 <seascape-map inline-template ref="map">
-
     <div>
         <div class="container-fluid" id="mapContainer">
             <div class="row">
-                <div class="col-sm-5 col-md-4 col-lg-3 full-height text-light text-center py-5 shadow"
-                     id="sidebar">
+                <div class="col-md-3 full-height text-light text-center py-5 shadow"
+                     id="sidebar"
+                     v-bind:class="{ 'col-md-9': results != null }">
                     <form v-on:submit.prevent="searchLocation($event)" ref="searchForm">
                         <div class="form-group required">
                             <h2>Data Querying</h2>
@@ -17,6 +17,43 @@
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                         @csrf
                     </form>
+                    <div v-if="results != null">
+                        <table class="table table-hover">
+                            <tr>
+                                <td>Longitude</td>
+                                <td>@{{ results.long }}</td>
+                            </tr>
+                            <tr>
+                                <td>Latitude</td>
+                                <td>@{{ results.lat }}</td>
+                            </tr>
+                            <tr>
+                                <td>Wind Direction</td>
+                                <td>@{{ results.wind_direction ? results.wind_direction : 'N/A' }}</td>
+                            </tr>
+                            <tr>
+                                <td>Particulant Matter 10</td>
+                                <td>@{{ results.pm10 ? results.pm10 : 'N/A' }}</td>
+                            </tr>
+                            <tr>
+                                <td>Particulant Matter 2.5</td>
+                                <td>@{{ results.pm2_5 ? results.pm2_5 : 'N/A' }}</td>
+                            </tr>
+                            <tr>
+                                <td>Carbon Dioxide</td>
+                                <td>@{{ results.co ? results.co : 'N/A' }}</td>
+                            </tr>
+                            <tr>
+                                <td>Nitrogen Dioxide</td>
+                                <td>@{{ results.no2 ? results.no2 : 'N/A' }}</td>
+                            </tr>
+                            <tr>
+                                <td>Ozone</td>
+                                <td>@{{ results.o3 ? results.o3 : 'N/A' }}</td>
+                            </tr>
+                        </table>
+                        {{--@{{ results }}--}}
+                    </div>
 
                     @auth
                         <a href="{{ route('logout') }}" class="btn btn-secondary btn-block text-light position-absolute"
@@ -24,31 +61,9 @@
                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
                     @endauth
                 </div>
-                <div class="col-sm-7 col-md-8 col-lg-9 full-height" id="map">
+                <div class="col-md full-height" id="map">
                 </div>
             </div>
         </div>
-
-        <div class="modal show" tabindex="-1" role="dialog" v-if="results != null">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title">Modal title</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-body">
-                <pre>
-                    @{{ results }}
-                </pre>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-              </div>
-            </div>
-          </div>
-        </div>
-	</div>
-
+    </div>
 </seascape-map>
